548d45672bdba131d6b42b67c46d5c042c90f065
Satake <satake@objectfoo.com>
2016-03-13 12:06:19 -0700

    move to browserify/watchify

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,62 +1,74 @@
-(function (global) {
-	const Retrospective = global.Retrospective = global.Retrospective || {};
-	const items = ['item 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'item 2'];
-	const voteItems = [
-		{text: 'asdf', vote: 0},
-		{text: 'asdf asdf asd fasd', vote: 1}
-	];
+'use strict';
 
-	Retrospective.App = React.createClass({
-		getInitialState: function () {
-			return this.props.store.getState();
-		},
+import React from 'react';
 
-		render: function () {
-			const List = Retrospective.List;
-			const date = new Date().toJSON().replace(/T.+$/, '');
+export default function App(props) {
+	return (<div>App.js</div>);
+}
 
-			return (
-				<div>
-					<header>
-						<h1 className="page-header">Retrospective <span className="page-sub-header">ReactJs version</span></h1>
-						<p className="page-header__date">Date: <time>{date}</time></p>
-					</header>
+// module.exports = function (props) {
+// 	return (<div>App.js</div>);
+// };
 
-					<section>
-						<h2 className="visible-aria-only">What went well?</h2>
-						<div className="pure-form">
-							<input className="pure-input-1 input-no-border-radius" autoComplete="off" type="text" placeholder="What went well?" />
-							<List items={items} />
-						</div>
-					</section>
-
-					<section>
-						<h2 className="visible-aria-only">What needs improvement?</h2>
-						<div className="pure-form">
-							<input className="pure-input-1 input-no-border-radius" autoComplete="off" type="text" placeholder="What needs improvement?" />
-							<List items={voteItems} isVoting={true} />
-						</div>
-					</section>
-
-					<section>
-						<h2 className="visible-aria-only">What do we do next time?</h2>
-						<div className="pure-form">
-							<input className="pure-input-1 input-no-border-radius" autoComplete="off" type="text" placeholder="What do we do next time?" />
-							<List items={items} />
-						</div>
-					</section>
-
-					<footer className="page-footer">
-						<button className="pure-button button-default" type="button">Sort</button>
-						<button className="pure-button button-default" type="button">Printable</button>
-						<button className="pure-button button-default" type="button">Clear All</button>
-					</footer>
-				</div>
-			);
-		}
-	});
-
-	if (typeof exports !== 'undefined') {
-		module.exports = Retrospective.App;
-	}
-})(typeof global === 'undefined' ? window : global);
+// (function (global) {
+// 	const Retrospective = global.Retrospective = global.Retrospective || {};
+// 	const items = ['item 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'item 2'];
+// 	const voteItems = [
+// 		{text: 'asdf', vote: 0},
+// 		{text: 'asdf asdf asd fasd', vote: 1}
+// 	];
+//
+// 	Retrospective.App = React.createClass({
+// 		getInitialState: function () {
+// 			return this.props.store.getState();
+// 		},
+//
+// 		render: function () {
+// 			const List = Retrospective.List;
+// 			const date = new Date().toJSON().replace(/T.+$/, '');
+//
+// 			return (
+// 				<div>
+// 					<header>
+// 						<h1 className="page-header">Retrospective <span className="page-sub-header">ReactJs version</span></h1>
+// 						<p className="page-header__date">Date: <time>{date}</time></p>
+// 					</header>
+//
+// 					<section>
+// 						<h2 className="visible-aria-only">What went well?</h2>
+// 						<div className="pure-form">
+// 							<input className="pure-input-1 input-no-border-radius" autoComplete="off" type="text" placeholder="What went well?" />
+// 							<List items={items} />
+// 						</div>
+// 					</section>
+//
+// 					<section>
+// 						<h2 className="visible-aria-only">What needs improvement?</h2>
+// 						<div className="pure-form">
+// 							<input className="pure-input-1 input-no-border-radius" autoComplete="off" type="text" placeholder="What needs improvement?" />
+// 							<List items={voteItems} isVoting={true} />
+// 						</div>
+// 					</section>
+//
+// 					<section>
+// 						<h2 className="visible-aria-only">What do we do next time?</h2>
+// 						<div className="pure-form">
+// 							<input className="pure-input-1 input-no-border-radius" autoComplete="off" type="text" placeholder="What do we do next time?" />
+// 							<List items={items} />
+// 						</div>
+// 					</section>
+//
+// 					<footer className="page-footer">
+// 						<button className="pure-button button-default" type="button">Sort</button>
+// 						<button className="pure-button button-default" type="button">Printable</button>
+// 						<button className="pure-button button-default" type="button">Clear All</button>
+// 					</footer>
+// 				</div>
+// 			);
+// 		}
+// 	});
+//
+// 	if (typeof exports !== 'undefined') {
+// 		module.exports = Retrospective.App;
+// 	}
+// })(typeof global === 'undefined' ? window : global);
--- src/store.js
+++ src/store.js
@@ -1,164 +1,170 @@
-(function (global) {
-	const Retrospective = global.Retrospective = global.Retrospective || {};
+'use strict';
 
-	// Initial State
-	const initialState = {
-		isPrintable: false,
-		good: [],
-		bad: [],
-		next: []
-	};
+export default function Store(initialData) {
+	this.a = 'c';
+}
 
-	let state = initialState;
-
-	// Action Types
-	Retrospective.types = {
-		SORT: 'SORT',
-		ADD_ITEM: 'ADD_ITEM',
-		INCREMENT_VOTE: 'INCREMENT_VOTE',
-		DECREMENT_VOTE: 'DECREMENT_VOTE',
-		RESET: 'RESET',
-		SET_ITEM_TEXT: 'SET_ITEM_TEXT'
-	};
-
-	// Action creators
-	Retrospective.actions = {
-		sort: function sort(data) {
-			return {
-				type: Retrospective.types.SORT,
-				data: data
-			};
-		},
-
-		addItem: function addItem(data) {
-			return {
-				type: Retrospective.types.ADD_ITEM,
-				data: data
-			};
-		},
-
-		setItemText: function (data) {
-			return {
-				type: Retrospective.types.SET_ITEM_TEXT,
-				data: data
-			};
-		},
-
-		incrementVote: function incrementVote(data) {
-			return {
-				type: Retrospective.types.INCREMENT_VOTE,
-				data: data
-			};
-		},
-
-		decrementVote: function incrementVote(data) {
-			return {
-				type: Retrospective.types.DECREMENT_VOTE,
-				data: data
-			};
-		},
-
-		reset: function reset(data) {
-			return {
-				type: Retrospective.types.RESET
-			};
-		}
-	};
-
-	// Store
-	Retrospective.store = {
-		getState: function () {
-			return state;
-		},
-
-		dispatch: function (action) {
-			state = reduce(state, action);
-		}
-	};
-
-	// Reducer
-	function reduce(state, action) {
-		switch (action.type) {
-			case Retrospective.types.RESET:
-				return reset();
-			case Retrospective.types.ADD_ITEM:
-				return addItem(state, action.data);
-			case Retrospective.types.SET_ITEM_TEXT:
-				return setItemText(state, action.data);
-			case Retrospective.types.INCREMENT_VOTE:
-				return incrementVote(state, action.data);
-			case Retrospective.types.DECREMENT_VOTE:
-				return decrementVote(state, action.data);
-			case Retrospective.types.SORT:
-				return sortList(state, action.data);
-			default:
-				console.log('warning unknown action', action.type);
-				return state;
-		}
-		// emit new state message
-	}
-
-	// state transforms
-	function sortList(state, data) {
-		const which = data.id;
-		const newState = Object.assign({}, state);
-		newState[which].sort((a, b) => {
-			return a.vote - b.vote;
-		});
-
-		return newState;
-	}
-	function reset() {
-		return Object.assign({}, initialState);
-	}
-
-	function incrementVote(state, data) {
-		const newState = Object.assign({}, state);
-
-		newState.bad[data.idx].vote += 1;
-
-		return newState;
-	}
-
-	function decrementVote(state, data) {
-		const newState = Object.assign({}, state);
-		const item = newState.bad[data.idx];
-
-		item.vote = Math.max(0, item.vote - 1);
-
-		return newState;
-	}
-
-	function setItemText(state, data) {
-		const list = data.id;
-		const newText = data.text;
-		const idx = data.idx;
-		const newState = Object.assign({}, state);
-
-		newState[list][idx].text = newText;
-		return newState;
-	}
-
-	function addItem(state, data) {
-		const list = data.id;
-		const newState = Object.assign({}, state);
-		const newItem = {text: data.text};
-
-		if (data.vote !== undefined) {
-			newItem.vote = data.vote;
-		}
-
-		newState[list] = newState[list].concat(newItem);
-
-		return newState;
-	}
-
-	if (typeof module !== 'undefined') {
-		module.exports = {
-			store: Retrospective.store,
-			types: Retrospective.types,
-			actions: Retrospective.actions
-		};
-		module.exports = Retrospective;
-	}
-})(typeof global === 'undefined' ? window : global);
+// (function (global) {
+// 	const Retrospective = global.Retrospective = global.Retrospective || {};
+//
+// 	// Initial State
+// 	const initialState = {
+// 		isPrintable: false,
+// 		good: [],
+// 		bad: [],
+// 		next: []
+// 	};
+//
+// 	let state = initialState;
+//
+// 	// Action Types
+// 	Retrospective.types = {
+// 		SORT: 'SORT',
+// 		ADD_ITEM: 'ADD_ITEM',
+// 		INCREMENT_VOTE: 'INCREMENT_VOTE',
+// 		DECREMENT_VOTE: 'DECREMENT_VOTE',
+// 		RESET: 'RESET',
+// 		SET_ITEM_TEXT: 'SET_ITEM_TEXT'
+// 	};
+//
+// 	// Action creators
+// 	Retrospective.actions = {
+// 		sort: function sort(data) {
+// 			return {
+// 				type: Retrospective.types.SORT,
+// 				data: data
+// 			};
+// 		},
+//
+// 		addItem: function addItem(data) {
+// 			return {
+// 				type: Retrospective.types.ADD_ITEM,
+// 				data: data
+// 			};
+// 		},
+//
+// 		setItemText: function (data) {
+// 			return {
+// 				type: Retrospective.types.SET_ITEM_TEXT,
+// 				data: data
+// 			};
+// 		},
+//
+// 		incrementVote: function incrementVote(data) {
+// 			return {
+// 				type: Retrospective.types.INCREMENT_VOTE,
+// 				data: data
+// 			};
+// 		},
+//
+// 		decrementVote: function incrementVote(data) {
+// 			return {
+// 				type: Retrospective.types.DECREMENT_VOTE,
+// 				data: data
+// 			};
+// 		},
+//
+// 		reset: function reset(data) {
+// 			return {
+// 				type: Retrospective.types.RESET
+// 			};
+// 		}
+// 	};
+//
+// 	// Store
+// 	Retrospective.store = {
+// 		getState: function () {
+// 			return state;
+// 		},
+//
+// 		dispatch: function (action) {
+// 			state = reduce(state, action);
+// 		}
+// 	};
+//
+// 	// Reducer
+// 	function reduce(state, action) {
+// 		switch (action.type) {
+// 			case Retrospective.types.RESET:
+// 				return reset();
+// 			case Retrospective.types.ADD_ITEM:
+// 				return addItem(state, action.data);
+// 			case Retrospective.types.SET_ITEM_TEXT:
+// 				return setItemText(state, action.data);
+// 			case Retrospective.types.INCREMENT_VOTE:
+// 				return incrementVote(state, action.data);
+// 			case Retrospective.types.DECREMENT_VOTE:
+// 				return decrementVote(state, action.data);
+// 			case Retrospective.types.SORT:
+// 				return sortList(state, action.data);
+// 			default:
+// 				console.log('warning unknown action', action.type);
+// 				return state;
+// 		}
+// 		// emit new state message
+// 	}
+//
+// 	// state transforms
+// 	function sortList(state, data) {
+// 		const which = data.id;
+// 		const newState = Object.assign({}, state);
+// 		newState[which].sort((a, b) => {
+// 			return a.vote - b.vote;
+// 		});
+//
+// 		return newState;
+// 	}
+// 	function reset() {
+// 		return Object.assign({}, initialState);
+// 	}
+//
+// 	function incrementVote(state, data) {
+// 		const newState = Object.assign({}, state);
+//
+// 		newState.bad[data.idx].vote += 1;
+//
+// 		return newState;
+// 	}
+//
+// 	function decrementVote(state, data) {
+// 		const newState = Object.assign({}, state);
+// 		const item = newState.bad[data.idx];
+//
+// 		item.vote = Math.max(0, item.vote - 1);
+//
+// 		return newState;
+// 	}
+//
+// 	function setItemText(state, data) {
+// 		const list = data.id;
+// 		const newText = data.text;
+// 		const idx = data.idx;
+// 		const newState = Object.assign({}, state);
+//
+// 		newState[list][idx].text = newText;
+// 		return newState;
+// 	}
+//
+// 	function addItem(state, data) {
+// 		const list = data.id;
+// 		const newState = Object.assign({}, state);
+// 		const newItem = {text: data.text};
+//
+// 		if (data.vote !== undefined) {
+// 			newItem.vote = data.vote;
+// 		}
+//
+// 		newState[list] = newState[list].concat(newItem);
+//
+// 		return newState;
+// 	}
+//
+// 	if (typeof module !== 'undefined') {
+// 		module.exports = {
+// 			store: Retrospective.store,
+// 			types: Retrospective.types,
+// 			actions: Retrospective.actions
+// 		};
+// 		module.exports = Retrospective;
+// 	}
+// })(typeof global === 'undefined' ? window : global);
