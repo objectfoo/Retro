8bb4d1461853f45f58a60bfbf85bcfd0787c8a0b
Satake <satake@objectfoo.com>
2016-03-13 18:35:29 -0700

    Add another smoke test for App component

--- test/App_spec.js
+++ test/App_spec.js
@@ -9,14 +9,21 @@ const scryRenderedDOMComponentsWithClass = ReactTestUtils.scryRenderedDOMCompone
 const App = require('../src/Components/App');
 const initialData = {text: 'lala'};
 
-describe.only('App', () => {
+describe('App', () => {
 	const component = renderIntoDocument(<App />);
 
 	it('should render div with lala content', () => {
 		const headline = scryRenderedDOMComponentsWithClass(component, 'page-header');
-
 		expect(headline).to.have.deep.property(
 			'[0].textContent',
 			'Retrospective w/ReactJs');
+	});
+
+	it('should have 1 input for each of the 3 sections', () => {
+		const inputs = scryRenderedDOMComponentsWithTag(component, 'input');
+		expect(inputs).to.have.property('length', 3);
+		expect(inputs[0].placeholder).to.equal('What went well?');
+		expect(inputs[1].placeholder).to.equal('What needs improvement?');
+		expect(inputs[2].placeholder).to.equal('What do we do next time?');
 	});
 });
